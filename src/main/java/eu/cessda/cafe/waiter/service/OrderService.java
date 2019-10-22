8f315453dbdbbe2055fa86940da953a4c19e5b98
Joshua Ocansey <joshua.ocansey@cessda.eu>
2019-10-22 09:50:34 +0200

    remove client connect from OrderService

--- src/main/java/eu/cessda/cafe/waiter/service/OrderService.java
+++ src/main/java/eu/cessda/cafe/waiter/service/OrderService.java
@@ -5,22 +5,21 @@ package eu.cessda.cafe.waiter.service;
  */
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import eu.cessda.cafe.waiter.data.model.Machines;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import eu.cessda.cafe.waiter.data.model.Order;
 import eu.cessda.cafe.waiter.database.DatabaseClass;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-class OrderService {
+public class OrderService {
 
 	private static final Logger log = LogManager.getLogger(OrderService.class);
 
@@ -32,23 +31,8 @@ class OrderService {
 		throw new UnsupportedOperationException();
 	}
 
-/* Engine method to retrieve data from cashier
- * TO BE DONE
- */
 
-    private static Machines machine = new Machines();
-    Client client = ClientBuilder.newClient();
-    private static final String cashierUrl = machine.getCashier();
-
-    public  Order getOrderId(){
-
-        return client
-                .target(cashierUrl)
-                .request(MediaType.APPLICATION_JSON)
-                .header("content-type", MediaType.APPLICATION_JSON)
-                .get(Order.class);
-
-    }
+    
 
     // Returns all orders from from cashier
 	public List<Order> getOrder(){
@@ -56,34 +40,25 @@ class OrderService {
 	}
 
 /* Returns responses for specific order based on conditions
- * TO BE REVIEWED by Matthew
+ * 
  */
-	public Order getSpecificOrder() {
+	public Response getSpecificOrder(String orderId) {
 
-		for (Entry<String, Order> pair : orderList.entrySet()) {
-			log.debug("key: {}, value: {}", pair.getKey(), pair.getValue());
-
-			log.info(" this is to test the method");
-
-		}
-
-		return null;
+		// Iterate through the object 	and return message based orderId, OrderSize and OrderDelivered TimeStamp
+		String condition1 = " Order Unknown";
+		String condition2 = " Order Not Ready";
+		String condition3 = " Order Already Delivered";
 		
-		/*
-		for(int i = 0; i < orderList.size(); i++) {		
-			if (order.orderId.equals(orderId)) {
-				if (order.orderDelivered.equals(null) || order.ordersize ==  order.coffees.getProduct().length) {
-					
-				}
-			} 	
-			
-		}
+		boolean ans = orderList.containsValue(orderId);
 		
-	*/
+	    Iterator<Map.Entry<String, Order>> orderIt = orderList.entrySet().iterator();
+	    
+	    return null;
 
+	   }
 
 
 
 	}
 
-}
+
