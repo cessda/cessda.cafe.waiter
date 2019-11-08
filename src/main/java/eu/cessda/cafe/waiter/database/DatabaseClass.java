a54a167ded09b06bdc0dbe30ab13a2623c52c09f
Matthew Morris <matthew.morris@cessda.eu>
2019-11-08 11:18:31 +0100

    Add copyright notice to all source code files.

--- src/main/java/eu/cessda/cafe/waiter/database/DatabaseClass.java
+++ src/main/java/eu/cessda/cafe/waiter/database/DatabaseClass.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright CESSDA ERIC 2019.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 package eu.cessda.cafe.waiter.database;
 
 /*
- * HashMap to collect and hold data in Java Memory 
+ * HashMap to collect and hold data in Java Memory
  */
 
 import eu.cessda.cafe.waiter.data.model.Job;
@@ -15,27 +30,26 @@ import java.util.UUID;
 
 public class DatabaseClass {
 
-	private DatabaseClass() {
-		throw new IllegalStateException("Utility class");
-	}
+    public static final Map<String, Machines> machine = new HashMap<>();
+    public static final Map<String, Order> order = new HashMap<>();
+    public static final Map<UUID, Job> job = new HashMap<>();
+    public static final Map<String, OrderHistory> orderHistory = new HashMap<>();
 
-	public static final Map<String, Machines> machine = new HashMap<>();
-	public static final Map<String, Order> order = new HashMap<>();
-	public static final Map<UUID, Job> job = new HashMap<>();
-	public static final Map<String, OrderHistory> orderHistory = new HashMap<>();
+    private DatabaseClass() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static Map<String, Order> getOrder() {
+        return order;
+    }
+
+    public static Map<String, Machines> getMachines() {
+        return machine;
+    }
 
 
-	public static Map<String, Order> getOrder() {
-		return order;
-	}
-
-	public static Map<String, Machines> getMachines() {
-		return machine;
-	}
-
-
-	public static Map<String, OrderHistory> getOrderHistory() {
-		return orderHistory;
-	}
+    public static Map<String, OrderHistory> getOrderHistory() {
+        return orderHistory;
+    }
 
 }
